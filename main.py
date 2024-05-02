188bbbb2e5e3140a7ca8f85b0d320e6db7721091
Kitwscw <159850948+Kitwscw@users.noreply.github.com>
2024-05-02 12:25:57 +1200

    made code check answer

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ print(f"Hello, {name} & welcome to this quiz")
 print("This quiz is about world history")
 #ask user a question
 print("Your first question is,")
-input("When did the first world war start")
-#tell user correct answer
-print("The corrrect answer is 1914")
-print("if you got it correct, then congratulations!")
-print("if you didnt then sorry")
+q1 = input("When did the first world war start")
+#check if user was correct, then tell user correct answer
+if q1=="1914":
+    print("Correct! the first world war started in 1914")
+elif q1!="1914":
+    print("Incorrect, the first world war started in 1914")
 #end quiz
 print("anyways thats the end of the quiz")
 print("have a nice day!")
