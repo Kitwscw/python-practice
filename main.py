10be4fc8117a05b32c8809c003e95abf004a7868
Kitwscw <159850948+Kitwscw@users.noreply.github.com>
2024-05-02 12:09:42 +1200

    made quiz v.1

---
+++ main.py
@@ -0,0 +1,15 @@
+#ask users name & save
+name=input("whats your name?")
+#greet user & introduce quiz
+print(f"Hello, {name} & welcome to this quiz")
+print("This quiz is about world history")
+#ask user a question
+print("Your first question is,")
+input("When did the first world war start")
+#tell user correct answer
+print("The corrrect answer is 1914")
+print("if you got it correct, then congratulations!")
+print("if you didnt then sorry")
+#end quiz
+print("anyways thats the end of the quiz")
+print("have a nice day!")
